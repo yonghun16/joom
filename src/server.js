7397d0b70b6385fb952d80cbe895a21e2ed106c0
yonghun16 <yonghun16@naver.com>
2025-02-23 01:09:07 +0900

    Frontend setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
-import express, { application } from "express";
+import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListener = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListener);
