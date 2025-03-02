004c14a9a3c31bedbb889514793e4e0582ff3df7
yonghun16 <yonghun16@naver.com>
2025-03-03 00:38:21 +0900

    카메라 선택

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,28 +1,54 @@
-const socket = io();
+const socket = io(); // socket.io
 
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");  // 카메라 선택
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
+// 카메라 선택
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;     // 카메라 device id
+      option.innerText = camera.label;    // 카메라 이름
+      camerasSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// 미디어 스크립
 async function getMedia() {
+  // 유저의 미디어 디바이스 정보 받아오기
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
       video: true,
+      audio: true,
     });
     myFace.srcObject = myStream;
-  } catch (e) {
-    console.log(e);
+    await getCameras();
+  } catch (error) {
+    console.log(error);
   }
 }
 
 getMedia();
 
+
+// 음소거(toggle)
 function handleMuteClick() {
+  myStream
+    // 오디오 정보 받기
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -31,7 +57,13 @@ function handleMuteClick() {
     muted = false;
   }
 }
+
+// 카메라 끄고 켜기(toggle)
 function handleCameraClick() {
+  myStream
+    // 비디오 정보 받기
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
